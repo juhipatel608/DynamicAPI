71a60382f3efabab59245077c7fd94700c54a5df
Your Name <juhipatel608@gmail.com>
2018-03-20 13:30:14 -0500

    cleaned code and finished

--- script.js
+++ script.js
@@ -1,87 +1,56 @@
+$(document).on("click", ".btn", function () {
+    var APIkey = "3GfICFcl1VDiA0O0Jz0Hjp45zKGZCzdp";
+    var search = $(this).attr("data-name")
 
-    //console.log(search)
-    //$(document).on("click", ".btn", sendAPIcall)
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + APIkey
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
 
-    $(".btn").on("click", function () {
-        var APIkey = "3GfICFcl1VDiA0O0Jz0Hjp45zKGZCzdp";
-        var search = $(this).attr("data-name")
-       
-        //var gif = $()
-        //var hero = $(this).attr("button-list");
-        
-       // var gif = "images"
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + APIkey
+        var results = response.data;
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-               
-                var results = response.data;
+        console.log(response);
+        for (var i = 0; i < results.length; i++) {
+            var imageStillURL = results[i].images.fixed_height_still.url;
+            var imageURL = results[i].images.fixed_height.url;
+            var HeroDiv = $("<div>");
+            var HeroImage = $("<img>").addClass("heroimage");
+            HeroImage.attr("src", imageStillURL);
+            HeroImage.attr("data-still", imageStillURL);
+            HeroImage.attr("data-animate", imageURL);
+            HeroImage.attr("data-state", "still");
+            var p = $("<p>").text("Rating:PG " + results[i].rating);
+            HeroImage.attr("alt", "heroimage");
+            HeroDiv.append(HeroImage);
+            HeroDiv.append(p);
+            $("#images-show").prepend(HeroDiv);
 
-                //console.log(queryURL);
-                console.log(response);
-                for (var i = 0; i < results.length; i++) {
-                    var imageStillURL = results[i].images.fixed_height_still.url;
-                    var imageURL = results[i].images.fixed_height.url;
-                    var HeroDiv = $("<div>").addClass("heroimage");
-                    var HeroImage = $("<img>");
-                    HeroImage.attr("src", imageStillURL);
-                    HeroImage.attr("data-still", imageStillURL);
-                    HeroImage.attr("data-animate", imageURL);
-                    HeroImage.attr("data-state", "still");
-                    var p = $("<p>").text("Rating:PG " + results[i].rating);
-                    HeroImage.attr("alt", "heroimage");
-                    HeroDiv.append(HeroImage);
-                    HeroDiv.append(p);
-                    $("#images-show").prepend(HeroDiv);
+        };
+    });
+});
 
-                };
-            });
-        });
+$("#moreButton").on("click", function (event) {
 
-        $("#add-item").on("click", function (event) {
+    event.preventDefault();
+    var search = $("#more").val().trim();
+    var newButton = $("<button>");
+    newButton.addClass("btn");
+    newButton.attr("data-name", search)
+    newButton.text(search)
+    $(".button-list").append(newButton);
+});
 
-            event.preventDefault();
-            var search = $("#search-input").val().trim();
-            topics.push(search);
+$(document).on("click", ".heroimage", function () {
 
-            createButton();
-        });
-
-        function addCategory(morehero) {
-            var newbtn = $('<div>').attr({
-                class: 'btn'
-            }).data({ text: morehero }).text(morehero);
-
-            $('button-list').append(newbtn)
-        }    
-
-    //var imgStillURL = response.data[i].images.fixed_height_still.url;
-
-    //var imgAnimateURL = response.data[i].images.fixed_height.url;
-    //HeroImage.attr("src", results[i].images.fixed_height.url);
-
-    function createButton() {
-        $("#button-hero").empty();
-
-        var a = $("<button>");
-
-        a.addClass("hero-btn");
-        a.attr("data-name", hero[i]);
-        a.text(hero[i]);
-        $("#button-hero").append(a);
-
-    };
-
-    $(".gif").on("click", function () {
-        var state = $(this).attr("data-state");
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-        }
-    })
+    var state = $(this).attr("data-state");
+    console.log($(this).attr("data-animate"));
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+})
